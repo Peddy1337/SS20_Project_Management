a08099b3134136a625cbb2154f18819eb0775fb0
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-13 18:21:35 +0200

    [impl] add logbook writing to json

--- json_reader_writer.py
+++ json_reader_writer.py
@@ -34,6 +34,52 @@ class JsonReaderWriter :
                 data = json.load(infile) # return dict when file is not empty
                 infile.close()
                 return data
+
+    def writeRideToJson(self,name,date,startKm,endKm,drivenKm,typeOfRide,purpose,startTime,endTime,accepted) :
+        contents = {}
+        contents['rides'] = []
+        contents['rides'].append({
+            'Name' : name,
+            'Datum' : date,
+            'Abfangskilometerstand' : startKm,
+            'Endkilometerstand' : endKm,
+            'gefahrene Kilometer' : drivenKm,
+            'Art der Fahrt' : typeOfRide,
+            'Zweck der Fahrt' : purpose,
+            'Fahrtanfang' : startTime,
+            'Fahrtende' : endTime,
+            'Bestaetigt' : accepted
+        })
+        with open(self.file, 'r+') as outfile :
+            if os.path.getsize(self.file) == 0:
+                outfile.close();
+                print('Logbook Header not set\n')
+                return False # Logbook Header has to be set before documenting rides
+            else :
+                data = json.load(outfile)
+                data['rides'].extend(contents['rides'])
+                outfile.seek(0)
+                json.dump(data, outfile, indent=4)
+                outfile.close()
+
+    def writeLogbookHeaderToJson(self,startDate,endDate,startKm,endKm,licensePlate) :
+        contents = {}
+        contents['header'] = []
+        contents['header'].append({
+            'Anfangsdatum' : startDate,
+            'Enddatum' : endDate,
+            'Abfangskilometerstand' : startKm,
+            'Endkilometerstand' : endKm,
+            'KFZ-Kennzeichen' : licensePlate,
+        })
+        contents['rides'] = []
+        with open(self.file, 'a+') as outfile :
+            if os.path.getsize(self.file) == 0:
+                json.dump(contents, outfile, indent=4)
+                outfile.close();
+            else :
+                print ('Logbook Header already exists\n')
+                return False # it is not permitted to change the Logbook Header of an existing Logbook
         
     file = ''
 
