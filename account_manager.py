5bc472caa7763a46f9adc0c9eb1f99c808c752eb
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-14 15:39:19 +0200

    [comment] add comments to AccountManager

--- account_manager.py
+++ account_manager.py
@@ -6,13 +6,16 @@ class AccountManager :
         self.jsonRW = json_reader_writer.JsonReaderWriter(self.file)
         self.loadAccountList()
 
+    # load account list from file
     def loadAccountList(self) :
         self.accountList = self.jsonRW.readFromJson()
 
+    # add account to file and load new list afterwards
     def addAccount(self,accDetails) :
         self.jsonRW.writeAccountToFile(accDetails['name'],accDetails['picture'],accDetails['pin'],accDetails['adress'])
         self.loadAccountList()
 
+    # edit an account and overwrite file
     def changeAccount(self,targetAccName,accDetails) :
         search = self.searchAccount(targetAccName)
         if search :
@@ -22,6 +25,7 @@ class AccountManager :
             print('Account couldnt be edited')
             return False
 
+    # search account by 'name' attribute index
     def searchAccount(self,targetAccName) :
         for a in self.accountList['accounts'] :
             if a['name'] == targetAccName :
@@ -30,6 +34,7 @@ class AccountManager :
         print('Account not found')
         return False
 
+    # deletes account and overwrite file
     def deleteAccount(self,targetAccName) :
         search = self.searchAccount(targetAccName)
         if search :
@@ -39,6 +44,7 @@ class AccountManager :
             print('Account couldnt be deleted')
             return False
 
+    # select account for login return false if account doesn't exists
     def selectAccount(self,targetAccName) :
         search = self.searchAccount(targetAccName)
         if search :
@@ -48,11 +54,13 @@ class AccountManager :
             self.deselectAccount()
             print('Account couldnt be selected')
             return False
-        
+
+    # deselect account for logout        
     def deselectAccount(self) :
         self.selectedAccount = {}
         self.accountSelected = False
 
+    # check if pin matches the account pin
     def checkPin(self,inputPin) :
         if self.accountSelected :
             return inputPin == self.selectedAccount['pin']
