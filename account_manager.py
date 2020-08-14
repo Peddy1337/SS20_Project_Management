5e39f9676eb5bd5e01ac834f16f53280dbf3c64e
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-14 15:30:47 +0200

    [fix] fix proper use of member variables

--- account_manager.py
+++ account_manager.py
@@ -16,16 +16,16 @@ class AccountManager :
     def changeAccount(self,targetAccName,accDetails) :
         search = self.searchAccount(targetAccName)
         if search :
-            accountList['accounts'][search] = accDetails
+            self.accountList['accounts'][search] = accDetails
             self.jsonRW.overwriteAccountList(self.accountList)
         else :
             print('Account couldnt be edited')
             return False
 
     def searchAccount(self,targetAccName) :
-        for a in accountList['accounts'] :
-            if p['name'] == targetAccName :
-                return p
+        for a in self.accountList['accounts'] :
+            if a['name'] == targetAccName :
+                return self.accountList['accounts'].index(a)
         # if account doesn't exist    
         print('Account not found')
         return False
@@ -33,7 +33,7 @@ class AccountManager :
     def deleteAccount(self,targetAccName) :
         search = self.searchAccount(targetAccName)
         if search :
-           del accountList['accounts'][search]
+           del self.accountList['accounts'][search]
            self.jsonRW.overwriteAccountList(self.accountList)
         else :
             print('Account couldnt be deleted')
@@ -42,16 +42,16 @@ class AccountManager :
     def selectAccount(self,targetAccName) :
         search = self.searchAccount(targetAccName)
         if search :
-            accountSelected = True
-            selectedAccount = accountList['accounts'][search]
+            self.accountSelected = True
+            self.selectedAccount = self.accountList['accounts'][search]
         else :
             self.deselectAccount()
             print('Account couldnt be selected')
             return False
         
     def deselectAccount(self) :
-        selectedAccount = {}
-        accountSelected = False
+        self.selectedAccount = {}
+        self.accountSelected = False
 
     def checkPin(self,inputPin) :
         if self.accountSelected :
