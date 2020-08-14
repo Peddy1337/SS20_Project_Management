db99fc1c335900dc04b8f2c7ca7d9e7174bfd033
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-14 16:49:10 +0200

    [impl] add prototype for route simulation

---
+++ route_simulator.py
@@ -0,0 +1,30 @@
+import threading
+import time
+
+class RouteSimulator :
+    def __init__(self) :
+        self.routeKm = 0
+        self.sleepTime = 5
+        self.running = False
+        self.simThread = threading.Thread(target = self.simulate , args=())
+        self.tlock() = threading.Lock()
+
+    def __del__(self) :
+        self.stopThread()
+
+    def startThread(self) :
+        self.running = True
+        self.simThread.start()
+
+    def stopThread(self) :
+        self.running = False
+        self.simThread.join()
+        self.routeKm = 0
+
+    def simulate(self) :
+        while self.running :
+            self.tlock.acquire()
+            self.routeKm += 0.1 # this is equal to 72 km/h
+            self.tlock.release()
+            time.sleep(self.sleepTime)
+            
