79b8b4b61cf8affd56ffa022d788795bbbbfd7b0
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-14 14:48:45 +0200

    [impl] add account list loading feature

--- account_manager.py
+++ account_manager.py
@@ -4,6 +4,11 @@ class AccountManager :
     def __init__(self,file) :
         self.file = file
         self.jsonRW = json_reader_writer.JsonReaderWriter(self.file)
+        self.loadAccountList()
+
+    def loadAccountList(self) :
+        self.accountList = self.jsonRW.readFromJson()
+
 
     file = ''
     accountList = {}
