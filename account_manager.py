75d7c6ddfba821f26e6a63ecabd2261c1dbc003b
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-14 14:52:03 +0200

    [impl] add account editing and searching feature

--- account_manager.py
+++ account_manager.py
@@ -13,6 +13,23 @@ class AccountManager :
         self.jsonRW.writeAccountToFile(accDetails['name'],accDetails['picture'],accDetails['pin'],accDetails['adress'])
         self.loadAccountList()
 
+    def changeAccount(self,targetAccName,accDetails) :
+        search = self.searchAccount(targetAccName)
+        if search :
+            accountList['accounts'][search] = accDetails
+            self.jsonRW.overwriteAccountList(self.accountList)
+        else :
+            print('Account couldnt be edited')
+            return False
+
+    def searchAccount(self,targetAccName) :
+        for a in accountList['accounts'] :
+            if p['name'] == targetAccName :
+                return p
+        # if account doesn't exist    
+        print('Account not found')
+        return False
+
 
     file = ''
     accountList = {}
