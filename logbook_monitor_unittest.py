b90ec5605bc14febafcdd66d326220e8c1010350
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-19 11:58:47 +0200

    [impl] add unit test for logbook_monitor

---
+++ logbook_monitor_unittest.py
@@ -0,0 +1,76 @@
+import logbook_monitor
+
+import time
+
+logbook = logbook_monitor.LogbookMonitor('test.json')
+
+logbook.updateDate()
+
+print (logbook.date)
+
+print (logbook.getTime())
+
+
+logbook.documentHeader('5370.0','WWP399')
+
+logbook.loadLogbook()
+
+print(logbook.logbook)
+
+print ('--------------------------')
+
+
+logbook.setDriverName('Bastian')
+
+logbook.setTypeOfRide('Privat')
+
+logbook.setPurpose('Privat')
+
+logbook.newRide()
+
+time.sleep(11)
+
+logbook.endRide()
+
+logbook.setSignature(False)
+
+logbook.documentRide()
+
+logbook.loadLogbook()
+
+print(logbook.logbook)
+
+print ('--------------------------')
+
+
+logbook.setDriverName('Patrick')
+
+logbook.setTypeOfRide('Geschaeftlich')
+
+logbook.setPurpose('Kundentermin Firma RAC')
+
+logbook.newRide()
+
+time.sleep(6)
+
+logbook.endRide()
+
+logbook.setSignature(True)
+
+logbook.documentRide()
+
+logbook.loadLogbook()
+
+print(logbook.logbook)
+
+print ('--------------------------')
+
+
+if logbook.checkUnsignedRides() :
+    logbook.signAllUnsignedRides()
+
+logbook.loadLogbook()
+
+print(logbook.logbook)
+
+print ('--------------------------')
