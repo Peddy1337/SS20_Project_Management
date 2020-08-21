fccd64f83e97cfb0e9ac10dd12e3c6d63b5b1954
JasminMS <64090058+JasminMS@users.noreply.github.com>
2020-08-21 11:41:51 +0200

    [impl] add new window staff selection, staff administration and admin menu

---
+++ Profilauswahl.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'dasDing.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 480)
+        MainWindow.setStyleSheet("QWidget{\n"
+"    background-color: #404040;\n"
+"    color: #00CC00}\n"
+"QPushButton{\n"
+"    color: #FFFFCC; \n"
+"    background-color: #006600;\n"
+"    border-style: inset;\n"
+"    border-color:black;\n"
+"    border-width: 1px;}\n"
+"QLineEdit,QLabel{\n"
+"    color: #FFFFCC;\n"
+"    background-color: #4FC44F;\n"
+"    border-style: inset;\n"
+"    border-color:black;\n"
+"    border-width: 1px;}")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 370, 200, 50))
+        font = QtGui.QFont()
+        font.setPointSize(26)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(590, 370, 200, 50))
+        font = QtGui.QFont()
+        font.setPointSize(26)
+        self.pushButton_2.setFont(font)
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(670, 0, 113, 32))
+        font = QtGui.QFont()
+        font.setPointSize(17)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setObjectName("lineEdit")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setGeometry(QtCore.QRect(0, 0, 661, 361))
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 659, 359))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.listView = QtWidgets.QListView(self.scrollAreaWidgetContents)
+        self.listView.setGeometry(QtCore.QRect(0, 0, 661, 361))
+        self.listView.setObjectName("listView")
+        self.widget = QtWidgets.QWidget(self.scrollAreaWidgetContents)
+        self.widget.setGeometry(QtCore.QRect(0, 0, 660, 80))
+        self.widget.setObjectName("widget")
+        self.radioButton = QtWidgets.QRadioButton(self.widget)
+        self.radioButton.setGeometry(QtCore.QRect(10, 10, 80, 60))
+        self.radioButton.setIconSize(QtCore.QSize(30, 30))
+        self.radioButton.setObjectName("radioButton")
+        self.label = QtWidgets.QLabel(self.widget)
+        self.label.setGeometry(QtCore.QRect(130, 10, 200, 50))
+        font = QtGui.QFont()
+        font.setPointSize(17)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 28))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Admin"))
+        self.pushButton_2.setText(_translate("MainWindow", "Anmelden"))
+        self.lineEdit.setText(_translate("MainWindow", "Suche"))
+        self.radioButton.setText(_translate("MainWindow", "RadioButton"))
+        self.label.setText(_translate("MainWindow", "Name"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
