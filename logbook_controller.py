8956d19c11c1784606ca8db4d8a6b1148b359e76
Peddy1337 <64089550+Peddy1337@users.noreply.github.com>
2020-08-26 15:51:30 +0200

    [impl] add account management implementation

--- logbook_controller.py
+++ logbook_controller.py
@@ -17,3 +17,34 @@ class LogbookController :
 
     def LoadStartKmFromConfig() :
         return ''
+
+    def selectAccount(self,name) :
+        self.accManager.selectAccount(name)
+
+    def deselectAccount(self) :
+        self.accManager.deselectAccount()
+
+    def checkPin(self,pin) :
+        return self.accManager.checkPin(pin)
+
+    def addAccount(self,name,picture,pin,adress) :
+        accDetails ={
+            'name' : name,
+            'picture' : picture,
+            'pin' : pin,
+            'adress' : adress
+            }
+        self.accManager.addAccount(accDetails)
+
+    def editAccount(self,name,picture,pin,adress) :
+        accDetails ={
+            'name' : name,
+            'picture' : picture,
+            'pin' : pin,
+            'adress' : adress
+            }
+        self.accManager.changeAccount(accDetails)
+
+    def deleteAccount(self,name) :
+        self.accManager.deleteAccount(name)
+
